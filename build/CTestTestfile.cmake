159ecd6766df21a873cd0fa9005b43582d00a51c
enin11 <dominic_24comet@yahoo.com>
2019-03-31 10:04:08 -0700

    catkin_ws

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/marc/projects/ros_nav_stack/src
+# Build directory: /home/marc/projects/ros_nav_stack/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
